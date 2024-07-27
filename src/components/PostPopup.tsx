2a3db4c7c86cc56972f8d58ac106cfa434a7bafe
Neha Panda <112271756+Neha00011@users.noreply.github.com>
2024-07-27 13:01:06 +0000

    Add

--- src/components/PostPopup.tsx
+++ src/components/PostPopup.tsx
@@ -33,7 +33,7 @@ const PostPopup = (props: postType) => {
               <div className="sm:items-start">
                 <h1 onClick={() => props?.setPost(false)} className="cursor-pointer">X</h1>
                 <input onChange={(e) => setQuest(e.target.value)} placeholder="Start your question with Why, What, How, etc." className="w-full outline-none h-30" />
-                <button onClick={addQuestion} className="bg-blue-500 text-white rounded-full p-2 w-40 ml-80 mt-3">Add question</button>
+                <button onClick={addQuestion} className="bg-blue-500 text-white rounded-full p-2 w-40 ml-80 mt-3">Add Question</button>
               </div>
             </div>
           </div>
